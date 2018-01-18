3cf95b8b34a1784a07431635c0d8dc96c6517e04
Elena <separise@gmail.com>
2018-01-18 16:53:42 +0700

    Updated composer.json and ClientTest

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -39,331 +39,331 @@ class ClientTest extends TestCase
 
     public function testAddMultiSigAddress()
     {
-        $this->assertTrue($this->bitcoinRPC->addMultiSigAddress(1, [1]) === 'OK' && method_exists($this->bitcoinRPC, 'addMultiSigAddress'));
+        $this->assertTrue($this->bitcoinRPC->addMultiSigAddress(1, [1]));
     }
 
     public function testAddNode()
     {
-        $this->assertTrue($this->bitcoinRPC->addNode(1, 'add') === 'OK' && method_exists($this->bitcoinRPC, 'addNode'));
+        $this->assertTrue($this->bitcoinRPC->addNode(1, 'add'));
     }
 
     public function testBackupWallet()
     {
-        $this->assertTrue($this->bitcoinRPC->backupWallet(1) === 'OK' && method_exists($this->bitcoinRPC, 'backupWallet'));
+        $this->assertTrue($this->bitcoinRPC->backupWallet(1));
     }
 
     public function testCreateMultiSig()
     {
-        $this->assertTrue($this->bitcoinRPC->createMultiSig(1, [1]) === 'OK' && method_exists($this->bitcoinRPC, 'createMultiSig'));
+        $this->assertTrue($this->bitcoinRPC->createMultiSig(1, [1]));
     }
 
     public function testCreateRawTransaction()
     {
-        $this->assertTrue($this->bitcoinRPC->createRawTransaction(1, 1) === 'OK' && method_exists($this->bitcoinRPC, 'createRawTransaction'));
+        $this->assertTrue($this->bitcoinRPC->createRawTransaction(1, 1));
     }
 
     public function testDecodeRawTransaction()
     {
-        $this->assertTrue($this->bitcoinRPC->decodeRawTransaction('hex') === 'OK' && method_exists($this->bitcoinRPC, 'decodeRawTransaction'));
+        $this->assertTrue($this->bitcoinRPC->decodeRawTransaction('hex'));
     }
 
     public function testDumpPrivKey()
     {
-        $this->assertTrue($this->bitcoinRPC->dumpPrivKey('address') === 'OK' && method_exists($this->bitcoinRPC, 'dumpPrivKey'));
+        $this->assertTrue($this->bitcoinRPC->dumpPrivKey('address'));
     }
 
     public function testDumpWallet()
     {
-        $this->assertTrue($this->bitcoinRPC->dumpWallet('filename') === 'OK' && method_exists($this->bitcoinRPC, 'dumpWallet'));
+        $this->assertTrue($this->bitcoinRPC->dumpWallet('filename'));
     }
 
     public function testEncryptWallet()
     {
-        $this->assertTrue($this->bitcoinRPC->encryptWallet('passPhrase') === 'OK' && method_exists($this->bitcoinRPC, 'encryptWallet'));
+        $this->assertTrue($this->bitcoinRPC->encryptWallet('passPhrase'));
     }
 
     public function testGetAccount()
     {
-        $this->assertTrue($this->bitcoinRPC->getAccount('address') === 'OK' && method_exists($this->bitcoinRPC, 'getAccount'));
+        $this->assertTrue($this->bitcoinRPC->getAccount('address'));
     }
 
     public function testGetAccountAddress()
     {
-        $this->assertTrue($this->bitcoinRPC->getAccountAddress('account') === 'OK' && method_exists($this->bitcoinRPC, 'getAccountAddress'));
+        $this->assertTrue($this->bitcoinRPC->getAccountAddress('account'));
     }
 
     public function testGetAddedNodeInfo()
     {
-        $this->assertTrue($this->bitcoinRPC->getAddedNodeInfo('dns', [1]) === 'OK' && method_exists($this->bitcoinRPC, 'getAddedNodeInfo'));
+        $this->assertTrue($this->bitcoinRPC->getAddedNodeInfo('dns', [1]));
     }
 
     public function testGetAddressesByAccount()
     {
-        $this->assertTrue($this->bitcoinRPC->getAddressesByAccount('account') === 'OK' && method_exists($this->bitcoinRPC, 'getAddressesByAccount'));
+        $this->assertTrue($this->bitcoinRPC->getAddressesByAccount('account'));
     }
 
     public function testGetBestBlockHash()
     {
-        $this->assertTrue($this->bitcoinRPC->getBestBlockHash() === 'OK' && method_exists($this->bitcoinRPC, 'getBestBlockHash'));
+        $this->assertTrue($this->bitcoinRPC->getBestBlockHash());
     }
 
     public function testGetBlock()
     {
-        $this->assertTrue($this->bitcoinRPC->getBlock('hash') === 'OK' && method_exists($this->bitcoinRPC, 'getBlock'));
+        $this->assertTrue($this->bitcoinRPC->getBlock('hash'));
     }
 
     public function testGetBlockCount()
     {
-        $this->assertTrue($this->bitcoinRPC->getBlockCount() === 'OK' && method_exists($this->bitcoinRPC, 'getBlockCount'));
+        $this->assertTrue($this->bitcoinRPC->getBlockCount());
     }
 
     public function testGetBlockHash()
     {
-        $this->assertTrue($this->bitcoinRPC->getBlockHash('index') === 'OK' && method_exists($this->bitcoinRPC, 'getBlockHash'));
+        $this->assertTrue($this->bitcoinRPC->getBlockHash('index'));
     }
 
     public function testGetBlockNumber()
     {
-        $this->assertTrue($this->bitcoinRPC->getBlockNumber() === 'OK' && method_exists($this->bitcoinRPC, 'getBlockNumber'));
+        $this->assertTrue($this->bitcoinRPC->getBlockNumber());
     }
 
     public function testGetBlockTemplate()
     {
-        $this->assertTrue($this->bitcoinRPC->getBlockTemplate(['params']) === 'OK' && method_exists($this->bitcoinRPC, 'getBlockTemplate'));
+        $this->assertTrue($this->bitcoinRPC->getBlockTemplate(['params']));
     }
 
     public function testGetConnectionCount()
     {
-        $this->assertTrue($this->bitcoinRPC->getConnectionCount() === 'OK' && method_exists($this->bitcoinRPC, 'getConnectionCount'));
+        $this->assertTrue($this->bitcoinRPC->getConnectionCount());
     }
 
     public function testGetDifficulty()
     {
-        $this->assertTrue($this->bitcoinRPC->getDifficulty() === 'OK' && method_exists($this->bitcoinRPC, 'getDifficulty'));
+        $this->assertTrue($this->bitcoinRPC->getDifficulty());
     }
 
     public function testGetGenerate()
     {
-        $this->assertTrue($this->bitcoinRPC->getGenerate() === 'OK' && method_exists($this->bitcoinRPC, 'getGenerate'));
+        $this->assertTrue($this->bitcoinRPC->getGenerate());
     }
 
     public function testGetHashesPerSec()
     {
-        $this->assertTrue($this->bitcoinRPC->getHashesPerSec() === 'OK' && method_exists($this->bitcoinRPC, 'getHashesPerSec'));
+        $this->assertTrue($this->bitcoinRPC->getHashesPerSec());
     }
 
     public function testGetInfo()
     {
-        $this->assertTrue($this->bitcoinRPC->getInfo() === 'OK' && method_exists($this->bitcoinRPC, 'getInfo'));
+        $this->assertTrue($this->bitcoinRPC->getInfo());
     }
 
     public function testGetMiningInfo()
     {
-        $this->assertTrue($this->bitcoinRPC->getMiningInfo() === 'OK' && method_exists($this->bitcoinRPC, 'getMiningInfo'));
+        $this->assertTrue($this->bitcoinRPC->getMiningInfo());
     }
 
     public function testListAccounts()
     {
-        $this->assertTrue($this->bitcoinRPC->listAccounts(1) === 'OK' && method_exists($this->bitcoinRPC, 'listAccounts'));
+        $this->assertTrue($this->bitcoinRPC->listAccounts(1));
     }
 
     public function testGetNewAddress()
     {
-        $this->assertTrue($this->bitcoinRPC->getNewAddress('account') === 'OK' && method_exists($this->bitcoinRPC, 'getNewAddress'));
+        $this->assertTrue($this->bitcoinRPC->getNewAddress('account'));
     }
 
     public function testGetPeerInfo()
     {
-        $this->assertTrue($this->bitcoinRPC->getPeerInfo() === 'OK' && method_exists($this->bitcoinRPC, 'getPeerInfo'));
+        $this->assertTrue($this->bitcoinRPC->getPeerInfo());
     }
 
     public function testGetRawChangeAddress()
     {
-        $this->assertTrue($this->bitcoinRPC->getRawChangeAddress('account') === 'OK' && method_exists($this->bitcoinRPC, 'getRawChangeAddress'));
+        $this->assertTrue($this->bitcoinRPC->getRawChangeAddress('account'));
     }
 
     public function testGetRawMemPool()
     {
-        $this->assertTrue($this->bitcoinRPC->getRawMemPool() === 'OK' && method_exists($this->bitcoinRPC, 'getRawMemPool'));
+        $this->assertTrue($this->bitcoinRPC->getRawMemPool());
     }
 
     public function testGetRawTransaction()
     {
-        $this->assertTrue($this->bitcoinRPC->getRawTransaction('txid', 1) === 'OK' && method_exists($this->bitcoinRPC, 'getRawTransaction'));
+        $this->assertTrue($this->bitcoinRPC->getRawTransaction('txid', 1));
     }
 
     public function testGetReceivedByAccount()
     {
-        $this->assertTrue($this->bitcoinRPC->getReceivedByAccount('account', 1) === 'OK' && method_exists($this->bitcoinRPC, 'getReceivedByAccount'));
+        $this->assertTrue($this->bitcoinRPC->getReceivedByAccount('account', 1));
     }
 
     public function testGetReceivedByAddress()
     {
-        $this->assertTrue($this->bitcoinRPC->getReceivedByAddress('address', 1) === 'OK' && method_exists($this->bitcoinRPC, 'getReceivedByAddress'));
+        $this->assertTrue($this->bitcoinRPC->getReceivedByAddress('address', 1));
     }
 
     public function testGetTransaction()
     {
-        $this->assertTrue($this->bitcoinRPC->getTransaction('txid') === 'OK' && method_exists($this->bitcoinRPC, 'getTransaction'));
+        $this->assertTrue($this->bitcoinRPC->getTransaction('txid'));
     }
 
     public function testGetTxout()
     {
-        $this->assertTrue($this->bitcoinRPC->getTxout('txid', 1, true) === 'OK' && method_exists($this->bitcoinRPC, 'getTxout'));
+        $this->assertTrue($this->bitcoinRPC->getTxout('txid', 1, true));
     }
 
     public function testGetTxoutSetInfo()
     {
-        $this->assertTrue($this->bitcoinRPC->getTxoutSetInfo() === 'OK' && method_exists($this->bitcoinRPC, 'getTxoutSetInfo'));
+        $this->assertTrue($this->bitcoinRPC->getTxoutSetInfo());
     }
 
     public function testGetWork()
     {
-        $this->assertTrue($this->bitcoinRPC->getWork('data') === 'OK' && method_exists($this->bitcoinRPC, 'getWork'));
+        $this->assertTrue($this->bitcoinRPC->getWork('data'));
     }
 
     public function testHelp()
     {
-        $this->assertTrue($this->bitcoinRPC->help('command') === 'OK' && method_exists($this->bitcoinRPC, 'help'));
+        $this->assertTrue($this->bitcoinRPC->help('command'));
     }
 
     public function testImportPrivKey()
     {
-        $this->assertTrue($this->bitcoinRPC->importPrivKey('privKey', 'label', true) === 'OK' && method_exists($this->bitcoinRPC, 'importPrivKey'));
+        $this->assertTrue($this->bitcoinRPC->importPrivKey('privKey', 'label', true));
     }
 
     public function testInvalidateBlock()
     {
-        $this->assertTrue($this->bitcoinRPC->invalidateBlock('hash') === 'OK' && method_exists($this->bitcoinRPC, 'invalidateBlock'));
+        $this->assertTrue($this->bitcoinRPC->invalidateBlock('hash'));
     }
 
     public function testKeypoolRefill()
     {
-        $this->assertTrue($this->bitcoinRPC->keypoolRefill() === 'OK' && method_exists($this->bitcoinRPC, 'keypoolRefill'));
+        $this->assertTrue($this->bitcoinRPC->keypoolRefill());
     }
 
     public function testListAddressGroupings()
     {
-        $this->assertTrue($this->bitcoinRPC->listAddressGroupings() === 'OK' && method_exists($this->bitcoinRPC, 'listAddressGroupings'));
+        $this->assertTrue($this->bitcoinRPC->listAddressGroupings());
     }
 
     public function testListReceivedByAccount()
     {
-        $this->assertTrue($this->bitcoinRPC->listReceivedByAccount(1, 1) === 'OK' && method_exists($this->bitcoinRPC, 'listReceivedByAccount'));
+        $this->assertTrue($this->bitcoinRPC->listReceivedByAccount(1, 1));
     }
 
     public function testListTransactions()
     {
-        $this->assertTrue($this->bitcoinRPC->listTransactions('account', 1, 'from') === 'OK' && method_exists($this->bitcoinRPC, 'listTransactions'));
+        $this->assertTrue($this->bitcoinRPC->listTransactions('account', 1, 'from'));
     }
 
     public function testListUnspent()
     {
-        $this->assertTrue($this->bitcoinRPC->listUnspent(1,10) === 'OK' && method_exists($this->bitcoinRPC, 'listUnspent'));
+        $this->assertTrue($this->bitcoinRPC->listUnspent(1, 10));
     }
 
     public function testListLockUnspent()
     {
-        $this->assertTrue($this->bitcoinRPC->listLockUnspent() === 'OK' && method_exists($this->bitcoinRPC, 'listLockUnspent'));
+        $this->assertTrue($this->bitcoinRPC->listLockUnspent());
     }
 
     public function testLockUnspent()
     {
-        $this->assertTrue($this->bitcoinRPC->lockUnspent('unlock', [1]) === 'OK' && method_exists($this->bitcoinRPC, 'lockUnspent'));
+        $this->assertTrue($this->bitcoinRPC->lockUnspent('unlock', [1]));
     }
 
     public function testMove()
     {
-        $this->assertTrue($this->bitcoinRPC->move('from', 'to', 123, 1, 'comment') === 'OK' && method_exists($this->bitcoinRPC, 'move'));
+        $this->assertTrue($this->bitcoinRPC->move('from', 'to', 123, 1, 'comment'));
     }
 
     public function testSendFrom()
     {
-        $this->assertTrue($this->bitcoinRPC->sendFrom('from', 'to', 123, 1, 'comment', 'commentTo') === 'OK' && method_exists($this->bitcoinRPC, 'sendFrom'));
+        $this->assertTrue($this->bitcoinRPC->sendFrom('from', 'to', 123, 1, 'comment', 'commentTo'));
     }
 
     public function testSendMany()
     {
-        $this->assertTrue($this->bitcoinRPC->sendMany('from', 'to', 123, 'comment') === 'OK' && method_exists($this->bitcoinRPC, 'sendMany'));
+        $this->assertTrue($this->bitcoinRPC->sendMany('from', 'to', 123, 'comment'));
     }
 
     public function testSendRawTransaction()
     {
-        $this->assertTrue($this->bitcoinRPC->sendRawTransaction('string') === 'OK' && method_exists($this->bitcoinRPC, 'sendRawTransaction'));
+        $this->assertTrue($this->bitcoinRPC->sendRawTransaction('string'));
     }
 
     public function testSetAccount()
     {
-        $this->assertTrue($this->bitcoinRPC->setAccount('address', 'account') === 'OK' && method_exists($this->bitcoinRPC, 'setAccount'));
+        $this->assertTrue($this->bitcoinRPC->setAccount('address', 'account'));
     }
 
     public function testSetGenerate()
     {
-        $this->assertTrue($this->bitcoinRPC->setGenerate('generate', 5) === 'OK' && method_exists($this->bitcoinRPC, 'setGenerate'));
+        $this->assertTrue($this->bitcoinRPC->setGenerate('generate', 5));
     }
 
-     public function testSetTxFee()
+    public function testSetTxFee()
     {
-        $this->assertTrue($this->bitcoinRPC->setTxFee(123) === 'OK' && method_exists($this->bitcoinRPC, 'setTxFee'));
+        $this->assertTrue($this->bitcoinRPC->setTxFee(123));
     }
 
     public function testSignMessage()
     {
-        $this->assertTrue($this->bitcoinRPC->signMessage('address', 'message') === 'OK' && method_exists($this->bitcoinRPC, 'signMessage'));
+        $this->assertTrue($this->bitcoinRPC->signMessage('address', 'message'));
     }
 
     public function testSignRawTransaction()
     {
-        $this->assertTrue($this->bitcoinRPC->signRawTransaction('hex', 'message','keys') === 'OK' && method_exists($this->bitcoinRPC, 'signRawTransaction'));
+        $this->assertTrue($this->bitcoinRPC->signRawTransaction('hex', 'message', 'keys'));
     }
 
     public function testStop()
     {
-        $this->assertTrue($this->bitcoinRPC->stop() === 'OK' && method_exists($this->bitcoinRPC, 'stop'));
+        $this->assertTrue($this->bitcoinRPC->stop());
     }
 
     public function testSubmitBlock()
     {
-        $this->assertTrue($this->bitcoinRPC->submitBlock('hex','params') === 'OK' && method_exists($this->bitcoinRPC, 'submitBlock'));
+        $this->assertTrue($this->bitcoinRPC->submitBlock('hex', 'params'));
     }
 
     public function testListReceivedByAddress()
     {
-        $this->assertTrue($this->bitcoinRPC->listReceivedByAddress(1,5) === 'OK' && method_exists($this->bitcoinRPC, 'listReceivedByAddress'));
+        $this->assertTrue($this->bitcoinRPC->listReceivedByAddress(1, 5));
     }
 
     public function testListSinceBlock()
     {
-        $this->assertTrue($this->bitcoinRPC->listSinceBlock('hash','target') === 'OK' && method_exists($this->bitcoinRPC, 'listSinceBlock'));
+        $this->assertTrue($this->bitcoinRPC->listSinceBlock('hash', 'target'));
     }
 
     public function testSendToAddress()
     {
-        $this->assertTrue($this->bitcoinRPC->sendToAddress('address',123, 'comment', 'commentTo') === 'OK' && method_exists($this->bitcoinRPC, 'sendToAddress'));
+        $this->assertTrue($this->bitcoinRPC->sendToAddress('address', 123, 'comment', 'commentTo'));
     }
 
     public function testValidateAddress()
     {
-        $this->assertTrue($this->bitcoinRPC->validateAddress('address') === 'OK' && method_exists($this->bitcoinRPC, 'validateAddress'));
+        $this->assertTrue($this->bitcoinRPC->validateAddress('address'));
     }
 
     public function testVerifyMessage()
     {
-        $this->assertTrue($this->bitcoinRPC->verifyMessage('address', 'signature', 'message') === 'OK' && method_exists($this->bitcoinRPC, 'verifyMessage'));
+        $this->assertTrue($this->bitcoinRPC->verifyMessage('address', 'signature', 'message'));
     }
 
     public function testWalletLock()
     {
-        $this->assertTrue($this->bitcoinRPC->walletLock() === 'OK' && method_exists($this->bitcoinRPC, 'walletLock'));
+        $this->assertTrue($this->bitcoinRPC->walletLock());
     }
 
     public function testWalletPassPhrase()
     {
-        $this->assertTrue($this->bitcoinRPC->walletPassPhrase('passPhrase', 1000) === 'OK' && method_exists($this->bitcoinRPC, 'walletPassPhrase'));
+        $this->assertTrue($this->bitcoinRPC->walletPassPhrase('passPhrase', 1000));
     }
 
     public function testWalletPassPhraseChange()
     {
-        $this->assertTrue($this->bitcoinRPC->walletPassPhraseChange('oldPassPhrase', 'newPassPhrase') === 'OK' && method_exists($this->bitcoinRPC, 'walletPassPhraseChange'));
+        $this->assertTrue($this->bitcoinRPC->walletPassPhraseChange('oldPassPhrase', 'newPassPhrase'));
     }
 }
